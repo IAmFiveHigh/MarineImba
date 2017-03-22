9c10e5ace42b6022930c28027f233f706456d81b
我是五高你敢信 <745170149@qq.com>
2017-03-22 09:42:11 +0800

    begin Project

---
+++ MarineImba/MarineImba/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MarineImba
+//
+//  Created by 我是五高你敢信 on 2017/3/22.
+//  Copyright © 2017年 我是五高你敢信. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
